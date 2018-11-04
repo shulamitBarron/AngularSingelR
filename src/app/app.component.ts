6dd5842ae3ba0bb2cd7bc1ca7567f2d283a36b6e
TamarBraverman <t0583224053@gmail.com>
2018-11-04 10:08:23 +0200

    fggy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { HubConnection } from '@aspnet/signalr-client';
+import { Http } from '@angular/http';
 import swal from 'sweetalert2'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   private _hubConnection: HubConnection;
-  isClick=false;
-  ismif=false;
+  isClick = false;
+  ismif = false;
   nick = 'אלמוני';
   message = '';
   messages: string[] = [];
-
+  constructor(private http: Http) { }
   public sendMessage(): void {
     this._hubConnection
       .invoke('sendToAll', this.nick, this.message)
       .catch(err => console.error(err));
   }
-  open()
-  {
-    
+  public runProgram(): void {
+    this.http.get('https://angularaspnetcoresignalr.azurewebsites.net/api/runProgram').subscribe((response) => { });
   }
-  mifrat()
-  {
-    if(this.ismif==true)
-    {
-      this.ismif=false;
+  mifrat() {
+    if (this.ismif == true) {
+      this.ismif = false;
       return;
     }
+    else
+      this.ismif = true;
   }
   con() {
-    if(this.isClick==true)
-    {
-      this.isClick=false;
+    if (this.isClick == true) {
+      this.isClick = false;
 
       return;
     }
- swal({
-     
-      title: ':הכנס את שמך',
+    swal({
+
+      title: 'הכנס את שמך:',
       input: 'text',
       inputAttributes: {
         autocapitalize: 'off'
       },
+
+      customClass: 'h',
       showCancelButton: false,
       confirmButtonText: 'שלח',
       showLoaderOnConfirm: true,
       preConfirm: (login) => {
-        if(login)
-        this.nick=login;
+        if (login)
+          this.nick = login;
         else
-        this.nick='אלמוני';
-        this.isClick=true;
+          this.nick = 'אלמוני';
+        this.isClick = true;
       },
-    });
+    })
+    ;
 
     this._hubConnection = new HubConnection('https://angularaspnetcoresignalr.azurewebsites.net/chat');
     this._hubConnection
@@ -63,10 +65,11 @@ export class AppComponent  {
       .then(() => console.log('Connection started!'))
       .catch(err => console.log('Error while establishing connection :('));
 
-      this._hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
-        const text = `${nick}: ${receivedMessage}`;
-        this.messages.push(text);
-      });
+    this._hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
+      const text = `${nick}: ${receivedMessage}`;
+      this.messages.push(text);
+    });
 
-    }
+  }
 }
+// http://localhost:5000/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 
@@ -12,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
