9115d3bf3c9f2ed6f0525a74c24f456a18a9ec05
TamarBraverman <t0583224053@gmail.com>
2018-11-04 11:18:39 +0200

    gg

---
+++ dist/main.bundle.js
@@ -0,0 +1,258 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_lazy_route_resource lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+var escape = __webpack_require__("../../../../css-loader/lib/url/escape.js");
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "html { \r\n    background: url(" + escape(__webpack_require__("../../../../../src/images/web.jpg")) + ") no-repeat center center fixed;\r\n    height: 96vh;\r\n    background-size: 100% 100%;\r\n    overflow: hidden;\r\n\r\n}\r\na{\r\n    color: #fc8731;\r\n    margin-left: -3vh;\r\n    font-family: serif;\r\n}\r\na:hover{\r\n    color: red;\r\n}\r\n#connaction{\r\n    width: 30vw;\r\n    height: 30vh;\r\n    background-color:blue;\r\n}\r\n#input{\r\n    outline: none;\r\n    border-collapse: unset;\r\n    border-color: transparent;\r\n    overflow: hidden;\r\n    background: url(" + escape(__webpack_require__("../../../../../src/images/helping.png")) + ") no-repeat center;\r\n    border-radius: 74vh;\r\n    height: 20vh;\r\n    float: right;\r\n    margin-top: -19vh;\r\n    margin-right: 3vh;\r\n    width: 21vh;\r\n}\r\ninput:hover{\r\n   background-color: red; \r\n}\r\n#div{\r\n    border-radius: 7vh;\r\n    background-color: #06101c;\r\n    border-color: #fc8731;\r\n    border-style: double;\r\n    float: right;\r\n    margin-top: -60vh;\r\n    margin-right: 4vh;\r\n    height: 51vh;\r\n    width: 21vw;\r\n}\r\n\r\n.center-div\r\n{\r\n  top:50%;\r\n  left: 50%;\r\n  transform: translate3d(-50%,-50%, 0);\r\n  position: absolute;\r\n}\r\nh2{\r\ncolor: #fc8731;\r\n}\r\nlabel{\r\ncolor:#fc8731;\r\n}\r\nbutton{\r\n    background-color: #fc8731;\r\n    color:black;\r\n}\r\nspan{\r\n    color:#fc8731;  \r\n}\r\n#message\r\n{\r\n    background-color: #070f1c;\r\n    color: #fc8731;\r\n    border-color: #fc8731;\r\n    direction: rtl;\r\n}\r\n#start{\r\n    margin-top: 50vh;\r\n    position: center;\r\n    /* position: absolute; */\r\n    display: block;\r\n    margin: 379px auto;\r\n    text-align: center;\r\n    width: 21vh;\r\n    height: 10vh;\r\n    border-color: #fc8731;\r\n    background-color: #070f1c;\r\n    border-radius: 4vh;\r\n    /* color: #FF9800; */\r\n    color: #fc8731;\r\n    font-size: 3vh;\r\n    font-family: sans-serif;\r\n    outline: none;\r\n    margin-left: 104vh;\r\n}\r\n#start:hover{\r\n\r\n    color: red;\r\n\r\n}\r\n.sweet-alert{\r\n    background-color: #E91E63 !important;\r\n    display: none;\r\n    position: relative;\r\n    -ms-flex-direction: column;\r\n        flex-direction: column;\r\n    -ms-flex-pack: center;\r\n        justify-content: center;\r\n    width: 32em;\r\n    max-width: 100%;\r\n    padding: 1.25em;\r\n    border-radius: .3125em;\r\n    background: #E91E63 !important;\r\n    font-family: inherit;\r\n    font-size: 1rem;\r\n    box-sizing: border-box ;\r\n}\r\n.swal2-header{\r\n    direction: rtl !important;\r\n    background: red !important;\r\n    background-color: #E91E63 !important;\r\n}\r\n #mifrat {\r\n    position: absolute;\r\n    bottom: 205px;\r\n    margin-top: 535px;\r\n    top: -0;\r\n    margin-top: 58vh;\r\n    position: center;\r\n    display: block;\r\n    /* margin: 379px auto; */\r\n    text-align: center;\r\n    width: 21vh;\r\n    height: 10vh;\r\n    border-color: #fc8731;\r\n    background-color: #070f1c;\r\n    border-radius: 4vh;\r\n    color: #fc8731;\r\n    font-size: 3vh;\r\n    font-family: sans-serif;\r\n    outline: none;\r\n    margin-top: 81vh;\r\n    margin-left: 79vh;\r\n}\r\n#mifrat:hover{\r\n  \r\n    color: red;\r\n }\r\n#h2{\r\n    text-align: center;\r\n}\r\np{\r\n    color: white;\r\n    font-size: 2vh;\r\n    font-family: monospace;\r\n    text-align: center;\r\n}\r\n\r\n.swal2-container.in{\r\nbackground-color: #E91E63 !important;\r\n}\r\n\r\n.h{\r\n    background-color: #E91E63 !important;\r\n}\r\n#div2{\r\n    border-radius: 7vh;\r\n    background-color: #06101c;\r\n    border-color: #fc8731;\r\n    border-style: double;\r\n    float: right;\r\n    margin-top: -60vh;\r\n    margin-right: 4vh;\r\n    height: 60vh;\r\n    width: 27vw;\r\n    border-radius: 7vh;\r\n    background-color: #06101c;\r\n    border-color: #fc8731;\r\n    border-style: double;\r\n    float: right;\r\n    margin-top: -60vh;\r\n    margin-right: 142vh;\r\n    height: 32vh;\r\n    width: 21vw;\r\n}\r\n\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<html>\n\n<body>\n  <div>\n    <ul>\n      <li>\n        <h2>\n          <a target=\"_blank\" rel=\"noopener\" href=\"https://www.lifewire.com/what-is-a-file-2625878\">אודות קבצי מחשב</a>\n        </h2>\n      </li>\n      <li>\n        <h2>\n          <a target=\"_blank\" rel=\"noopener\" href=\"https://www.techlicious.com/tip/get-organized-and-clean-out-your-computer/\">פתרונות לעומס מיותר במחשב</a>\n        </h2>\n      </li>\n      <li>\n        <h2>\n          <a target=\"_blank\" rel=\"noopener\" href=\"https://www.scootersoftware.com/download.php\">תוכנות נוספות בתכום</a>\n        </h2>\n      </li>\n    </ul>\n  </div>\n  <div>\n    <h2>\n      <!-- <img class=\"img1\" src=\"../images/help.png\" > -->\n      <input id=\"input\" type=\"button\" (click)=\"con()\">\n    </h2>\n  </div>\n  <button id=\"start\" (click)=\"runProgram()\">הפעל את התוכנה</button>\n  <button id=\"mifrat\" (click)=\"mifrat()\">מפרט התוכנה</button>\n</body>\n\n</html>\n<div id=\"div\" *ngIf=\"isClick\">\n  <div id=\"main-container\" style=\"text-align:center\">\n    <div class=\"container\">\n      <h2>!שלום {{nick}} </h2>\n      <form (ngSubmit)=\"sendMessage()\" #chatForm=\"ngForm\">\n        <div>\n          <input type=\"text\" id=\"message\" name=\"message\" [(ngModel)]=\"message\" required>\n          <label for=\"message\">:הודעה</label>\n        </div>\n        <p></p>\n        <p></p>\n        <button type=\"submit\" id=\"sendmessage\" [disabled]=\"!chatForm.valid\">\n          שלח\n        </button>\n      </form>\n    </div>\n    <p></p>\n    <p></p>\n    <div class=\"container\" *ngIf=\"messages.length > 0\">\n      <div *ngFor=\"let message of messages\">\n        <span>{{message}}</span>\n      </div>\n    </div>\n  </div>\n</div>\n<div id=\"div2\" *ngIf=\"ismif\">\n  <h2 id=\"h2\">מפרט התוכנה</h2>\n  <p></p>\n  <p></p>\n  <p></p>\n  <p>עליך להוריד את התוכנה שנמצאת בלינק של תוכנות נוספות בתכום ולאחר מכן ללחוץ על הכפתור הפעל את התוכנה בתוך התוכנה ישנו הסבר על כל חלונית ע\"י לחיצה על מקש עזרה אם ברצונך לקבל מפרט המוסבר בהרחבה</p>\n  <h2 id=\"h2\">!!!!!!!!!!בהנאה</h2>\n  <p></p>\n  <p></p>\n  <p></p>\n  <p></p>\n  <p></p>\n  <p></p>\n</div>\n\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__aspnet_signalr_client__ = __webpack_require__("../../../../@aspnet/signalr-client/dist/src/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__aspnet_signalr_client___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__aspnet_signalr_client__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_http__ = __webpack_require__("../../../http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_sweetalert2__ = __webpack_require__("../../../../sweetalert2/dist/sweetalert2.all.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_sweetalert2___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_sweetalert2__);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+var AppComponent = (function () {
+    function AppComponent(http) {
+        this.http = http;
+        this.isClick = false;
+        this.ismif = false;
+        this.nick = 'אלמוני';
+        this.message = '';
+        this.messages = [];
+    }
+    AppComponent.prototype.sendMessage = function () {
+        this._hubConnection
+            .invoke('sendToAll', this.nick, this.message)
+            .catch(function (err) { return console.error(err); });
+    };
+    AppComponent.prototype.runProgram = function () {
+        this.http.get('https://angularaspnetcoresignalr.azurewebsites.net/api/runProgram').subscribe(function (response) { });
+    };
+    AppComponent.prototype.mifrat = function () {
+        if (this.ismif == true) {
+            this.ismif = false;
+            return;
+        }
+        else
+            this.ismif = true;
+    };
+    AppComponent.prototype.con = function () {
+        var _this = this;
+        if (this.isClick == true) {
+            this.isClick = false;
+            return;
+        }
+        __WEBPACK_IMPORTED_MODULE_3_sweetalert2___default()({
+            title: 'הכנס את שמך:',
+            input: 'text',
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            customClass: 'h',
+            showCancelButton: false,
+            confirmButtonText: 'שלח',
+            showLoaderOnConfirm: true,
+            preConfirm: function (login) {
+                if (login)
+                    _this.nick = login;
+                else
+                    _this.nick = 'אלמוני';
+                _this.isClick = true;
+            },
+        });
+        this._hubConnection = new __WEBPACK_IMPORTED_MODULE_1__aspnet_signalr_client__["HubConnection"]('https://angularaspnetcoresignalr.azurewebsites.net/chat');
+        this._hubConnection
+            .start()
+            .then(function () { return console.log('Connection started!'); })
+            .catch(function (err) { return console.log('Error while establishing connection :('); });
+        this._hubConnection.on('sendToAll', function (nick, receivedMessage) {
+            var text = nick + ": " + receivedMessage;
+            _this.messages.push(text);
+        });
+    };
+    AppComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__("../../../../../src/app/app.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__angular_http__["a" /* Http */]])
+    ], AppComponent);
+    return AppComponent;
+}());
+
+// http://localhost:5000/ 
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_forms__ = __webpack_require__("../../../forms/esm5/forms.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("../../../http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_2__angular_core__["E" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_1__angular_forms__["a" /* FormsModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_http__["b" /* HttpModule */]
+            ],
+            providers: [],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false
+};
+
+
+/***/ }),
+
+/***/ "../../../../../src/images/helping.png":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "helping.4e79537f0c1d15ae89b4.png";
+
+/***/ }),
+
+/***/ "../../../../../src/images/web.jpg":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "web.c6d7058cfe7bca83134a.jpg";
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/esm5/platform-browser-dynamic.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_7" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ }),
+
+/***/ 1:
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
